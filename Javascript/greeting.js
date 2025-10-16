a5211ff41865998aa6ad12ffd26d8304eb7a4f5d
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-10-15 21:03:40 -0700

    first practice with JS for FCC

---
+++ Javascript/greeting.js
@@ -0,0 +1,38 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot ;
+bot = "professorBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself");
+
+let botIntroduction = "My name is " + bot + ".";
+
+console.log(botIntroduction)
+
+let botLocationSentence = "I live in " + botLocation + ".";
+
+console.log(botLocationSentence);
+
+let nicknameIntroduction = "My nickname is " + bot +".";
+
+console.log(nicknameIntroduction);
+
+//change the bot response 
+bot = "awesomeTeacherBot";
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+
+console.log(newNicknameGreeting);
+
+let favoriteSubject;
+favoriteSubject = "Computer Science";
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+//final message
+console.log("Well, it was nice to talk to you. Have a nice day!");
+
+
