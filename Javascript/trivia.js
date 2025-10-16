39e4dfbfa22b4e6d306c5a1191eaf4e54840d477
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-10-16 14:50:27 -0700

    first draft of the trivia project

---
+++ Javascript/trivia.js
@@ -0,0 +1,77 @@
+//log "Hello!  I'm your coding fun fact guide!"
+
+/* create three variables: 
+
+botName
+botLocation
+codingFact
+favoriteLanguage*/
+//also for codingFact use let
+
+
+//log the botName and botLocation to the console  "My name is (botName) and I live on (botLocation)"
+
+
+/* log "My favorite programming language is (favoriteLanguage)." */
+
+
+
+const botname = "murray"; 
+const botlocation = "anaheim, ca"; 
+
+let favoritelanguage;
+favoritelanguage = "ruby";
+let codingfact; 
+codingfact = "yukihiro 'matz' matsumoto";
+
+
+console.log("hello! i'm your coding fun fact guide!");
+console.log("my name is " + botname + " and i live on " + botlocation + ".");
+console.log("my favorite programming language is " + favoritelanguage + "."); 
+console.log("The inventor " + codingFact + " invented " + favoriteLanguage + ".");
+
+
+
+//expanding on codingFact variable
+codingFact = favoriteLanguage + " was invented in 1995";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is a general-purpose, object-oriented programming language known for its simplicity, flexibility, and productivity. It was created by Yukihiro Matsumoto in 1995 with the goal of combining the best features of other programming languages like Perl, Smalltalk, and Lisp. Ruby is widely used across various domains due to its versatility and developer-friendly syntax.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is still popular in the world, particularly in the context of web development, especially through Ruby on Rails (RoR).";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you.  Goodbye! - " + botName + " from " + botLocation+ ".");
+//assign codingFact a new fact and say an interesting fact about the favoriteLanguage variable   * coding fact will be outputted to the console three times.
+
+
+const botName = "Murray"; 
+const botLocation = "Anaheim, CA"; 
+
+let favoriteLanguage;
+favoriteLanguage = "Ruby";
+let codingFact; 
+codingFact = "Yukihiro 'Matz' Matsumoto";
+
+
+console.log("Hello! I'm your coding fun fact guide!");
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + "."); 
+console.log("The inventor " + codingFact + " invented " + favoriteLanguage + ".");
+
+
+
+//expanding on codingFact variable
+codingFact = favoriteLanguage + " was invented in 1995";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is a general-purpose, object-oriented programming language known for its simplicity, flexibility, and productivity. It was created by Yukihiro Matsumoto in 1995 with the goal of combining the best features of other programming languages like Perl, Smalltalk, and Lisp. Ruby is widely used across various domains due to its versatility and developer-friendly syntax.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is still popular in the world, particularly in the context of web development, especially through Ruby on Rails (RoR).";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you.  Goodbye! - " + botName + " from " + botLocation+ ".");
