4c2c7de41682c46b1032038db2a1702c9143faba
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-11-05 14:56:08 -0800

    precitice

--- Javascript/emailPractice.js
+++ Javascript/emailPractice.js
@@ -2,13 +2,28 @@ const str1 = "doej2babdfasldkfjal;sdkjfa;lskdjf;alskdjfla;ksdjgl;aksdjgl;kashdgl
 
 // console.log(str1.endsWith("best!"));
 // Expected output: true
-
-function wordSearch(str1) {
     let key = "@";
-    let keyFind = str1.indexOf(key) - 1;
-    let keySlice = str1.slice(1,keyFind);
-    let keyReplace = str1.replace(keySlice,"*");
-    let keyRepeat = str1.repeat(keyFind + keyReplace);
-    return console.log(keyRepeat);
+    let keyFind1 = str1.indexOf(key) - 1;
+    let keyFind2 = str1.indexOf(1);
+    var keyFindTotal = keyFind1 + keyFind2;
+function keyFind(str1) {
+    let key = "@";
+    let keyFind1 = str1.indexOf(key) - 1;
+    let keyFind2 = str1.indexOf(1);
+    var keyFindTotal = keyFind1 + keyFind2;
+    // // let keySlice = str1.slice(1,keyFind);
+    // let keyReplace = str1.replace(keyFind,"*");
+    // // let keyRepeat = 
+    // // let keyRepeat = str1.repeat(keyFind + keyReplace);
+    return console.log(keyFindTotal);
 }
-wordSearch(str1);
+
+function wordSwap(str1) {
+    let wordKey = keyFind(str1);
+    let wordStart = "X".repeat(1);
+    let wordEnd = "X".repeat(wordKey);
+    console.log(wordStart + wordEnd);
+    
+}
+keyFind(str1);
+wordSwap(str1);
