143c2eb459923942dccc3a017716ba62309fb79e
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-11-26 09:43:39 -0800

    revised the leapYear program

--- Javascript/leapYear.js
+++ Javascript/leapYear.js
@@ -1,12 +1,12 @@
 // let result = '';
-let year = '1990';
+let year = '2024';
 
 function isLeapYear(year) {
-    if (year % 4) {
+    if (year/4 && %4) {
         return console.log (`${year} is a leap year.`);
-    } else if (year % 400){
+    } else if (year %= 400){
         return console.log (`${year} is a leap year.`);
-    } else if (year % 100) {
+    } else if (year %= 100) {
         return console.log (`${year} is not a leap year.`);
     }
     else {
@@ -15,6 +15,6 @@ function isLeapYear(year) {
 }
 // let result = year;
 
-console.log(isLeapYear(year));
+console.log(isLeapYear());
 
 // need to use the modulo operator
