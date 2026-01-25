81d8c6c9727e3da6ce009d75d24bc555453b6514
Eulersghost <learner_studios@proton.me>
2026-01-25 12:11:22 -0800

    Auto-commit

--- Javascript/factorial.js
+++ Javascript/factorial.js
@@ -9,6 +9,7 @@ Pseudocode
 4. method defined that takes the number times num - 1 the method goes all the way down until 0.
 5. return `Factorial of ${num} is ${result}`
 
+this is something that I'm working on right now and it's not too bad but there are still a lot of things that 
 */
 function factorialCalculator(num){
 
