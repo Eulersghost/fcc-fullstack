149abcd6e2aaed695d47f34a02a81ea215310832
Jason Palacio <jpalacio@evergreenhealthcare.org>
2026-01-27 17:34:07 -0800

    made some changes to the pseudocode and working on making sure the factorial calculation is done.

--- Javascript/factorial.js
+++ Javascript/factorial.js
@@ -1,17 +1,28 @@
-//write a function that will calculate factorial calculations 
+//write a function that will calculate factorial calculations
 
 /*
-Pseudocode 
+New pseudocode: 
 
-1. define function
-2. inside the function, create a variable to hold the value stored when the loop is iterated
-3. loop iterates over the length of the number passed in the parameter.
-4. method defined that takes the number times num - 1 the method goes all the way down until 0.
-5. return `Factorial of ${num} is ${result}`
-
-this is something that I'm working on right now and it's not too bad but there are still a lot of things that 
-something else that I'm working on and it's pretty cool, I haven't ironed it out completely yet but I'll figure it out
+1. define variable num to store the numbers that we are computing
+2. put a number there that I will use to calculate the factorial
+3. setup factorial calculation, for example if it was 4!, then start from 1 and work my way up.
+    3a. Worked example: 
+    3b. num = 1, 
+4. use a for loop, for counting through the factorial range.
+5. create a resultMsg to store the template literal.
+6. when loop finishes print the result per the assignment directions using a template literal.
 */
-function factorialCalculator(num){
 
+function factorialCalculator(num){
+    let resultMsg = "";
+    let result = "";
+    for(let i=1; i<=num; i++){
+        num * num;
+        if (i === num){
+            return resultMsg = `The factorial ${num}! is the same`;
+        }
+        else {
+            return resultMsg = `The factorial ${num} is ${num}'`;
+        }
+}
 }
