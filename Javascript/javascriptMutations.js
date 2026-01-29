b731c7553aa6776181889a14a2294b7945a9436e
Jason Palacio <jpalacio@evergreenhealthcare.org>
2026-01-28 19:17:47 -0800

    Auto-commit

---
+++ Javascript/javascriptMutations.js
@@ -0,0 +1,7 @@
+/* 1. compare the array before and after
+2. after arrays change compare it to the previous one
+3. if they're different output either true or false, 
+    in the freecodecamp example elements in both arrays match true if all there elements are the same, for example ["Hello", "hello"] = true, case sensitivity doesn't matter
+4. loop through each element in the array, sidenote probably use the length and split properties/methods
+5. 
+ */
