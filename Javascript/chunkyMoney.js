716afb5968a80b2ce1a2ba4c793ad287ee4f452e
Eulersghost <learner_studios@proton.me>
2026-05-18 20:37:38 -0700

    jus testing some things out

--- Javascript/chunkyMoney.js
+++ Javascript/chunkyMoney.js
@@ -1,4 +1,4 @@
-function chunkArrayInGroups(array, size){
+        function chunkArrayInGroups(array, size){
     let result = [];
     for (let i =0; i< array.length; i += size){
        let arrayChunk = array.slice(i, i + size);
